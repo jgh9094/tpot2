70be69de73ad7af61fb7274477f432cb32169681
Jose <jgh9094@gmail.com>
2024-10-10 11:57:28 -0700

    coin flip to see if we mutate a node or not

--- tpot2/search_spaces/pipelines/sequential.py
+++ tpot2/search_spaces/pipelines/sequential.py
@@ -21,19 +21,19 @@ class SequentialPipelineIndividual(SklearnIndividual):
             self.pipeline.append(space.generate(rng))
 
         self.pipeline = np.array(self.pipeline)
-        
+
     #TODO, mutate all steps or just one?
     def mutate(self, rng=None):
-        # mutated = False
-        # for step in self.pipeline:
-        #     if rng.random() < 0.5:
-        #         if step.mutate(rng):
-        #             mutated = True
-        # return mutated
         rng = np.random.default_rng(rng)
-        step = rng.choice(self.pipeline)
-        return step.mutate(rng)
-     
+        mutated = False
+        for step in self.pipeline:
+            if rng.random() < 0.5:
+                if step.mutate(rng):
+                    mutated = True
+        return mutated
+        # step = rng.choice(self.pipeline)
+        # return step.mutate(rng)
+
 
     def crossover(self, other, rng=None):
         #swap a random step in the pipeline with the corresponding step in the other pipeline
@@ -47,33 +47,33 @@ class SequentialPipelineIndividual(SklearnIndividual):
         for cx_func in cx_funcs:
             if cx_func(other, rng):
                 return True
-            
+
         return False
 
     def _crossover_swap_node(self, other, rng):
         if len(self.pipeline) != len(other.pipeline):
             return False
-        
-        
+
+
         rng = np.random.default_rng(rng)
         idx = rng.integers(1,len(self.pipeline))
 
         self.pipeline[idx], other.pipeline[idx] = other.pipeline[idx], self.pipeline[idx]
         return True
-    
+
     def _crossover_swap_multiple_nodes(self, other, rng):
 
         if len(self.pipeline) != len(other.pipeline):
             return False
-        
+
         if len(self.pipeline) < 2:
             return False
-    
+
         rng = np.random.default_rng(rng)
 
         max_steps = int(min(len(self.pipeline), len(other.pipeline))/2)
         max_steps = max(max_steps, 1)
-        
+
         if max_steps == 1:
             n_steps_to_swap = 1
         else:
@@ -83,17 +83,17 @@ class SequentialPipelineIndividual(SklearnIndividual):
 
         for idx in indexes_to_swap:
             self.pipeline[idx], other.pipeline[idx] = other.pipeline[idx], self.pipeline[idx]
-        
-        
+
+
         return True
 
     def _crossover_swap_segment(self, other, rng):
         if len(self.pipeline) != len(other.pipeline):
             return False
-        
+
         if len(self.pipeline) < 2:
             return False
-        
+
         rng = np.random.default_rng(rng)
         idx = rng.integers(1,len(self.pipeline))
 
@@ -104,16 +104,16 @@ class SequentialPipelineIndividual(SklearnIndividual):
             self.pipeline[idx:], other.pipeline[idx:] = other.pipeline[idx:], self.pipeline[idx:]
 
         return True
-    
+
     def _crossover_node(self, other, rng):
         rng = np.random.default_rng(rng)
-        
+
         # crossover_success = False
         # for idx in range(len(self.pipeline)):
         #     if rng.random() < 0.5:
         #         if self.pipeline[idx].crossover(other.pipeline[idx], rng):
         #             crossover_success = True
-                
+
         # return crossover_success
 
 
@@ -122,17 +122,17 @@ class SequentialPipelineIndividual(SklearnIndividual):
             if rng.random() < 0.5:
                 if self.pipeline[idx].crossover(other.pipeline[idx], rng):
                     crossover_success = True
-                
+
         return crossover_success
-    
+
     def export_pipeline(self, memory=None, **kwargs):
         return sklearn.pipeline.make_pipeline(*[step.export_pipeline(memory=memory, **kwargs) for step in self.pipeline], memory=memory)
-    
+
     def unique_id(self):
         l = [step.unique_id() for step in self.pipeline]
         l = ["SequentialPipeline"] + l
         return TupleIndex(tuple(l))
-    
+
 
 
 
@@ -141,7 +141,7 @@ class SequentialPipeline(SearchSpace):
         """
         Takes in a list of search spaces. will produce a pipeline of Sequential length. Each step in the pipeline will correspond to the the search space provided in the same index.
         """
-        
+
         self.search_spaces = search_spaces
 
     def generate(self, rng=None):
